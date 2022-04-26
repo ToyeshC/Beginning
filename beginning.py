f3aaecd0fa2dd84ac25b5cd2771a4fd55d9119a7
ToyeshC <toyesh.c@gmail.com>
2022-04-27 00:09:12 +0200

    password proper

--- beginning.py
+++ beginning.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import string
+import random
+
+choice = string.ascii_letters + string.digits + string.punctuation
+
+passwordList = random.sample(choice, 10)
+
+password = "".join(passwordList)
+
+print(password)
