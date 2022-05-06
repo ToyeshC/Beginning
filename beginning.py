adfb37dd73fde29cbca6e4ee466cb281ccc8eaa0
ToyeshC <toyesh.c@gmail.com>
2022-05-06 12:02:44 +0200

    ML begins

--- beginning.py
+++ beginning.py
@@ -1,4 +1,11 @@
-import string
-import random
+# import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-print("".join(random.sample(string.ascii_letters+string.digits+string.punctuation, 10)))
+#import dataset
+dataset = pd.read_csv("/Users/toyesh/Documents/Personal/Coding/Data Science/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/Data.csv")
+x = dataset.iloc[:, :-1].values        # features
+y = dataset.iloc[:, -1].values         # dependent variabe
+
+print(x, y, sep = "\n\n")
