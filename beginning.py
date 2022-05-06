ba7125d975dabada4a6e740afdd188acf64e5d23
ToyeshC <toyesh.c@gmail.com>
2022-05-06 13:34:40 +0200

    account for missing values

--- beginning.py
+++ beginning.py
@@ -4,8 +4,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #import dataset
-dataset = pd.read_csv("/Users/toyesh/Documents/Personal/Coding/Data Science/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/Data.csv")
+dataset = pd.read_csv("/Users/toyesh/Documents/Personal/Coding/Python/Data.csv")
 x = dataset.iloc[:, :-1].values        # features
-y = dataset.iloc[:, -1].values         # dependent variabe
+y = dataset.iloc[:, -1].values         # dependent variable
 
-print(x, y, sep = "\n\n")
+# missing data handling
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer.fit(x[:, 1:3])
+
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+print(x)
